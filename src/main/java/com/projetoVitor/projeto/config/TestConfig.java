ae0feb7f0e79c1bb86b4f3b841947fa855d8603b
Jose Vitor <josevitorbarroso123@gmail.com>
2025-01-14 19:00:24 -0300

    Implementacao do enum OrderStatus

--- src/main/java/com/projetoVitor/projeto/config/TestConfig.java
+++ src/main/java/com/projetoVitor/projeto/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.projetoVitor.projeto.config;
 
 import com.projetoVitor.projeto.entities.Order;
 import com.projetoVitor.projeto.entities.User;
+import com.projetoVitor.projeto.entities.enums.OrderStatus;
 import com.projetoVitor.projeto.repositories.OrderRepository;
 import com.projetoVitor.projeto.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User("maria", "maria123@gmail.com", "87643833", "12345");
         User u2 = new User("zezin", "zezin123@gmail.com", "99643833", "12785");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
