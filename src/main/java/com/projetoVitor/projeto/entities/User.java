2bb7e64f935670459d37ca366b5548b1b821b9a5
Jose Vitor <josevitorbarroso123@gmail.com>
2025-01-13 21:14:49 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/projetoVitor/projeto/entities/User.java
+++ src/main/java/com/projetoVitor/projeto/entities/User.java
@@ -3,6 +3,8 @@ package com.projetoVitor.projeto.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -64,6 +68,9 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
