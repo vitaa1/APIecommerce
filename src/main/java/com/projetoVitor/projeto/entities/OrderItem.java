9a65678cd1f7fd0565a88cdf5339549a669e1973
Jose Vitor <josevitorbarroso123@gmail.com>
2025-01-18 15:18:37 -0300

    entidade orderItem

---
+++ src/main/java/com/projetoVitor/projeto/entities/OrderItem.java
@@ -0,0 +1,57 @@
+package com.projetoVitor.projeto.entities;
+
+import com.projetoVitor.projeto.entities.pk.OrderItemPk;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+
+    @EmbeddedId
+    private final OrderItemPk id;
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        this.id = new OrderItemPk();
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- src/main/java/com/projetoVitor/projeto/resources/CategoryResource.java
+++ src/main/java/com/projetoVitor/projeto/resources/CategoryResource.java
@@ -32,5 +32,4 @@ public class CategoryResource {
         return ResponseEntity.ok().body(obj);
     }
 
-
 }
